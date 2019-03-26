ceb0f0d323853e416b4ee263c010e4e6ee7d80cc
pabloL0pez <pablo.lopez@contratosac.com.ar>
2019-03-26 10:39:44 -0300

    singleton SessionUtil

---
+++ src/util/SessionUtil.java
@@ -0,0 +1,26 @@
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionUtil {
+	
+	private static SessionUtil instance = new SessionUtil();
+	private SessionFactory factory;
+	
+	private SessionUtil() {
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+	}
+	
+	private static SessionUtil getInstance() {
+		return instance;
+	}
+
+	public static Session getSession() {
+		return getInstance().factory.openSession();
+	}
+}
